9ca6f5980e75d50515344edfacb91b87f72aa7a5
Ton Tan <tan120803@gmail.com>
2024-10-20 16:48:32 +0700

    OnGK

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';  // Đường dẫn có thể thay đổi tùy vào vị trí file LoginScreen.js
+import ElectronicsScreen from './screens/ElectronicsScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} // Ẩn header cho màn hình đăng nhập
+        />
+        <Stack.Screen 
+          name="Electronics" 
+          component={ElectronicsScreen} 
+          options={{ 
+            headerTitle: '',
+            headerBackTitleVisible: false, 
+            headerStyle: {
+              elevation: 0, 
+              shadowOpacity: 0, 
+              borderBottomWidth: 0, 
+            },
+            header: () => null,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
